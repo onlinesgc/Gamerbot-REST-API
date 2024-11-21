2a438579fdea87704f3f75e24b0b48b49c031271
Vermium Sifell <vermium@zyner.org>
2024-11-21 10:37:54 +0100

    simplify mongo connection

--- src/models/startup.ts
+++ src/models/startup.ts
@@ -4,9 +4,5 @@ import mongoose from "mongoose";
  * connect to mongo database
  */
 export const start_mongo_connection = async () => {
-  new Promise<void>((resolve) => {
-    mongoose.connect(process.env.MONGO_SRV as string, {}).then(() => {
-      resolve();
-    });
-  });
+  await mongoose.connect(process.env.MONGO_URL);
 };
