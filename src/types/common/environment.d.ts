d94679a766f320bd3c5c1b3705d9fd04dde0c0b1
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2025-12-04 10:10:59 +0100

    added more values to env declaration

--- src/types/common/environment.d.ts
+++ src/types/common/environment.d.ts
@@ -4,6 +4,9 @@ declare global {
         interface ProcessEnv {
             MONGO_URL: string;
             LOG_LEVEL: string;
+            MONGO_URL_TESTDATABASE: string;
+            PORT: string;
+            DEBUG: string;
         }
     }
 }
